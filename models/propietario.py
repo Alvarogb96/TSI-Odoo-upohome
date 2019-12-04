41003b60b48b51f3353f453e0a7ab189d0080097
Alvaro Gonzalez Barranco <alvgonbar96@gmail.com>
2019-12-04 05:26:20 -0800

    # WARNING: head commit changed in the meantime
    Añado cita a master

---
+++ models/propietario.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from odoo import fields,models
+
+
+class propietario(models.Model):
+    _name = 'upohome.propietario'
+    
+    name = fields.Char('DNI', size=9, required=True)
+    nombre = fields.Char('Nombre', size=60, required=True)
+    apellidos = fields.Char('Apellidos', size=60, required=True) 
+    telefono = fields.Char('Telefono', size=9, required=True)
