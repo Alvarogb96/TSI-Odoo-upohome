46da10e0afd7aa8e0e041a8f555dd958dfd1c96a
Carlos Carvajal <carvajalmorenocarlos@gmail.com>
2019-12-04 11:53:42 +0100

    Fecha fin añadida

--- models/alquiler.py
+++ models/alquiler.py
@@ -8,7 +8,7 @@ class alquiler(models.Model):
     
     name = fields.Char('ID alquiler', size=60, required=True)
     fechaInicio = fields.Datetime('Fecha de inicio', required=True)
-   
+    fechaFin = fields.Datetime('Fecha fin', required=True)
     cliente_id = fields.Many2one('upohome.cliente', 'Cliente') 
     vivienda_id = fields.Many2one('upohome.vivienda', 'Vivienda')
     
