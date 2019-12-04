12dcef08f9cceba1aad23a9fb2e92339607683d2
Alvaro Gonzalez Barranco <alvgonbar96@gmail.com>
2019-12-04 05:32:19 -0800

    Atributos de cita

--- models/cita.py
+++ models/cita.py
@@ -5,4 +5,8 @@ from odoo import fields,models
 
 class cita(models.Model):
     _name = 'upohome.cita'
+    
+    name = fields.Char('ID de la cita', size=9, required=True)
+    fecha = fields.Datetime('Fecha y hora',required=True)
+    descripcion = fields.Char('Descripcion', size=100)
 
