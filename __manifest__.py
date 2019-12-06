3b82b1cfa8b9c8852a3119b35b7f02e722109fd0
Alvaro Gonzalez Barranco <alvgonbar96@gmail.com>
2019-12-06 11:07:09 +0100

    Manifest actualizado con las vistas nuevas

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,7 @@
     'category': 'Upohome',
     'version': '0.1',
     'depends': ['base'],
-    'data': ['views/cliente_view.xml', 'views/alquiler_view.xml','views/vivienda_view.xml'],
+    'data': ['views/cliente_view.xml', 'views/alquiler_view.xml', 'views/vivienda_view.xml', 'views/cita_view.xml', 'views/empleado_view.xml', 'views/limpieza_view.xml', 'views/galeria_view.xml', 'views/reforma_view.xml', 'views/propietario_view.xml'],
     'demo': [ ],
     'application': True,
 }
